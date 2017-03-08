5ddbfe259cd3db2923f50af7321aa64c1ed416ea
Robert <robbyl@ovi.com>
2017-03-08 18:50:18 +0300

    update

--- save_ads.php
+++ save_ads.php
@@ -3,13 +3,20 @@
 require './config/database.php';
 require './functions/functions.php';
 
-if (move_uploaded_file($_FILES['ads']['tmp_name'], "../totara/theme/ilearn/pix/" . $_FILES['ads']['name'])) {
+$file_type = $_FILES['ads']['type'];
+$allowed_files = array("image/png", "image/gif", "image/jpeg");
 
-    $file_name = $_FILES['ads']['name'];
+if (in_array($file_type, $allowed_files)) {
+    if (move_uploaded_file($_FILES['ads']['tmp_name'], "../totara/theme/ilearn/pix/" . $_FILES['ads']['name'])) {
 
-    $query_update_photo = "UPDATE ads SET image = '{$file_name}' WHERE id = 1";
-    $result_update_photo = mysqli_query($link, $query_update_photo) or die(mysqli_error($link));
+        $file_name = $_FILES['ads']['name'];
+
+        $query_update_photo = "UPDATE ads SET image = '{$file_name}' WHERE id = 1";
+        $result_update_photo = mysqli_query($link, $query_update_photo) or die(mysqli_error($link));
+        echo "<img src='../totara/theme/ilearn/pix/{$file_name}' />";
+    }
+} else {
+    echo "Uploaded file type not supported";
 }
 
-echo "<img src='../totara/theme/ilearn/pix/{$file_name}' />";
 
